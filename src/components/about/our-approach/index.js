5f1d2dd43f70951d5998a299e91f01df4c7bb523
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-09-14 00:41:34 +0530

    about page done

--- src/components/about/our-approach/index.js
+++ src/components/about/our-approach/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./styles.css";
+import { Card, CardContent } from "@/components/ui/card";
 
 const AboutOurApproachComp = () => {
   return (
-    <div className=" overflow-hidden">
+    <div className="overflow-hidden">
       <section className="section-timeline space-y-8">
         <div className="flex flex-col gap-4 items-center text-center">
           <h1 className="text-center">Our Approach</h1>
@@ -16,7 +17,7 @@ const AboutOurApproachComp = () => {
         <div className="container ">
           <ul className="timeline">
             <li className="">
-              <div className="content">
+              <Card className="content gap-0">
                 <h3>User-Centric Design</h3>
                 <p>
                   We craft intuitive, accessible, and visually engaging
@@ -24,10 +25,10 @@ const AboutOurApproachComp = () => {
                   guided by usability and simplicity, ensuring a seamless
                   experience across devices and audiences.
                 </p>
-              </div>
+              </Card>
             </li>
             <li className="">
-              <div className="content">
+              <Card className="content gap-0">
                 <h3>Agile Development</h3>
                 <p>
                   We follow a sprint-based approach that ensures speed,
@@ -36,10 +37,10 @@ const AboutOurApproachComp = () => {
                   requirements, deliver faster results, and keep you involved at
                   every stage
                 </p>
-              </div>
+              </Card>
             </li>
             <li className="">
-              <div className="content">
+              <Card className="content gap-0">
                 <h3>Cloud-Native Solutions</h3>
                 <p>
                   We design and deploy applications optimized for AWS, Azure,
@@ -48,10 +49,10 @@ const AboutOurApproachComp = () => {
                   scaling, reduced costs, and future-ready infrastructure for
                   your business.
                 </p>
-              </div>
+              </Card>
             </li>
             <li className="">
-              <div className="content">
+              <Card className="content gap-0">
                 <h3>Data-Driven Insights</h3>
                 <p>
                   We leverage advanced analytics, AI, and automation to
@@ -59,10 +60,10 @@ const AboutOurApproachComp = () => {
                   empower businesses to make smarter, faster decisions, optimize
                   operations, and unlock new opportunities for growth
                 </p>
-              </div>
+              </Card>
             </li>
             <li className="">
-              <div className="content">
+              <Card className="content gap-0">
                 <h3>Cybersecurity Built-In</h3>
                 <p>
                   Security is at the core of everything we build. From threat
@@ -71,7 +72,7 @@ const AboutOurApproachComp = () => {
                   safeguarding against hackers, malware, and data breaches —
                   like a vaccine for your digital assets
                 </p>
-              </div>
+              </Card>
             </li>
           </ul>
         </div>
