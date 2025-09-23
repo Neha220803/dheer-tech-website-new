3795cd2588d2220eae93b15286855a572230ffa8
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-09-23 17:28:43 +0530

    nextjs re direct added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/about-us.php",
+        destination: "/about",
+        permanent: true, // 301 redirect
+      },
+      {
+        source: "/services.php",
+        destination: "/our-services",
+        permanent: true,
+      },
+      {
+        source: "/branding.php",
+        destination: "/our-services",
+        permanent: true,
+      },
+      {
+        source: "/contact-us.php",
+        destination: "/contact",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
